fa27dd8d6f39727bfa72f6d1881976c6d31c3a47
hoangdzvl1810 <hoangdzvl2005@gmail.com>
2026-05-19 14:07:33 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ function App() {
   return (
     <>
     <h3>Hello World</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>RoleNo</th>
+          <th>Full name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>HE19999</td>
+          <td>To AN An</td>
+        </tr>
+      </tbody>
+    </table>
     </>
   );
 }
